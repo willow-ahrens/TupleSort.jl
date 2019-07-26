f47bd21ee3ac24baa697956a7e1377c0d1e5de32
Peter Ahrens <ptrahrens@gmail.com>
2019-07-26 15:13:08 -0400

    fixed just for you alex

--- src/ripnetworks.jl
+++ src/ripnetworks.jl
@@ -1,8 +1,18 @@
 raw_html(n) = readlines(open(`curl http://jgamble.ripco.net/cgi-bin/nw.cgi?inputs=$n&algorithm=best&output=macro`))
-println(raw_html(10))
-println(raw_html(10))
-for line in raw_html(10)
-	for m in match(r"SWAP(\(\d+\), \(\d+\))", line)
-	    println(m)
+swaps = []
+for n = 2:64
+	swap = []
+	for line in raw_html(n)
+		for m in eachmatch(r"SWAP\((\d+), (\d+)\)", line)
+			push!(swap, (parse(Int, m[1]) + 1, parse(Int, m[2]) + 1))
+		end
 	end
+	push!(swaps, swap)
+end
+for swap in swaps
+	print("[")
+	for s in swap
+		print("$s, ")
+	end
+	println("],")
 end
