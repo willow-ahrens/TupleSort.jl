7e23e9a30477829ead0ddbb9eb849f7c87d9f3de
Peter Ahrens <ptrahrens@gmail.com>
2019-07-27 15:16:42 -0400

    ready for PR I think

--- src/bench.jl
+++ src/bench.jl
@@ -2,46 +2,37 @@ using BenchmarkTools
 using Plots
 using Random
 using TupleSort
-using ChipSort
+using ProgressMeter
+using BSON: @save, @load
 pyplot()
 
-#=
-function permutations(things)
-    if length(things) <= 1
-        return [things]
-    end
-    perms = []
-    for i in 1:length(things)
-        for rest in permutations((things[1:i - 1]..., things[i + 1:end]...))
-            push!(perms, (things[i], rest...))
-        end
-    end
-    return perms
-end
-=#
+const N = 64
 
-N = 64
-tuplesort_times = zeros(N)
-referencesort_times = zeros(N)
+newsort_times = zeros(N)
+copysort_times = zeros(N)
 arraysort_times = zeros(N)
 
-@generated function referencesort(stuff::NTuple{N}) where {N}
+@generated function copysort(stuff::NTuple{N}) where {N}
     return quote
         s = sort!(Base.copymutable(stuff))
         return ($([:(s[$n]) for n = 1:N]...),)
     end
 end
 
-nsamples = 20
-for n = 1:N
-    println(n)
-    tuplesort_times[n] += time(@benchmark sort(perm) setup = (perm = (randperm($n)...,)) samples = nsamples)
-    arraysort_times[n] += time(@benchmark sort(perm) setup = (perm = randperm($n)) samples = nsamples)
-    referencesort_times[n] += time(@benchmark referencesort(perm) setup = (perm = (randperm($n)...,)) samples = nsamples)
+if true
+    p = Progress(N, 1, "Making Ur Plot...")
+    for n = 1:N
+        newsort_times[n] += time(@benchmark sort(perm) setup=(perm=(randperm($n)...,))) - time(@benchmark identity(perm) setup=(perm=(randperm($n)...,)))
+        copysort_times[n] += time(@benchmark copysort(perm) setup=(perm=(randperm($n)...,))) - time(@benchmark identity(perm) setup=(perm=(randperm($n)...,)))
+        arraysort_times[n] += time(@benchmark sort!(perm) setup=(perm=randperm($n))) - time(@benchmark identity(perm) setup=(perm=randperm($n)))
+	next!(p)
+    end
+    @save "results.bson" newsort_times copysort_times arraysort_times # Same as above
 end
+@load "results.bson" newsort_times copysort_times arraysort_times # Same as above
 
-plot(0:N, [0, tuplesort_times...], label="TupleSort")
-plot!(0:N, [0, referencesort_times...], label="sort!")
-plot!(0:N, [0, arraysort_times...], label="arraysort!")
+plot(1:N, [newsort_times...], label="newsort(::Tuple{Vararg{Int}})", ylims=(0, max(maximum(newsort_times), maximum(copysort_times), maximum(arraysort_times))), xlabel="N", ylabel="time (ns)")
+plot!(1:N, [copysort_times...], label="copysort(::Tuple{Vararg{Int}})")
+plot!(1:N, [arraysort_times...], label="sort!(::Vector{Int})")
 
 savefig("plot.png")
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,13 @@ using Test
 using Random
 using TupleSort
 
-function permutations(things)
+function mypermutations(things)
     if length(things) <= 1
         return [things]
     end
     perms = []
     for i in 1:length(things)
-        for rest in permutations((things[1:i - 1]..., things[i + 1:end]...))
+        for rest in mypermutations((things[1:i - 1]..., things[i + 1:end]...))
             push!(perms, (things[i], rest...))
         end
     end
@@ -17,7 +17,7 @@ end
 
 for n = 0:8
     s = ((1:n)...,)
-    for t in permutations(s)
+    for t in mypermutations(s)
         @test sort(t) == s
         @test sort(t, by = -) == reverse(s)
         @test sort(t, lt = >) == reverse(s)
@@ -28,7 +28,7 @@ for n = 0:8
         @test sort(t, by = -, lt = >, rev=true) == reverse(s)
     end
 end
-for n = 9:32
+for n = 9:33
     for _ in 1:100
         s = ((1:n)...,)
         t = (randperm(n)...,)
@@ -42,3 +42,14 @@ for n = 9:32
         @test sort(t, by = -, lt = >, rev=true) == reverse(s)
     end
 end
+
+s = ((1:3)...,)
+@test typeof(sort(s)) == typeof(s)
+s = ((1:64)...,)
+@test typeof(sort(s)) == typeof(s)
+s = reverse(Float64.(((1:64)...,)))
+@test sort(s) == Float64.(((1:64)...,))
+@test typeof(sort(s)) == typeof(s)
+s = reverse(Float64.(((1:3)...,)))
+@test typeof(sort(s)) == typeof(s)
+@test sort(s) == Float64.(((1:3)...,))
